b56be7427e7c5a968addcad823de7d8c95db463f
marc122005 <marcamgad12@gmail.com>
2026-02-28 21:09:42 +0200

    add careers section

--- js/home.js
+++ js/home.js
@@ -1,2 +1,19 @@
 console.log("home js file is linked");
 
+
+let btnShowMore = document.getElementById("show-more");
+let moreContents = document.querySelectorAll(".more");
+
+btnShowMore.addEventListener("click", function(event){
+    event.preventDefault();
+
+    moreContents.forEach(function(box){
+        if (box.style.display === "block") {
+            box.style.display = "none";
+            btnShowMore.textContent = "Show More";
+        } else {
+            box.style.display = "block";
+            btnShowMore.textContent = "Show Less";
+        }
+    });
+});
