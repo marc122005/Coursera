e68d809fb15c254bb19b22895f7c6e5c352baadc
marc122005 <marcamgad12@gmail.com>
2026-03-01 03:17:13 +0200

    create logout button

--- js/home.js
+++ js/home.js
@@ -1,6 +1,25 @@
-console.log("home js file is linked");
+let logoutBtn = document.querySelector('.logout');
 
+// if user is logout , display the logout button and show the register button 
+logoutBtn.addEventListener('click', function() {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    location.reload();
+    logoutBtn.style.display = 'none';
+});
 
+// check if user is login or not , if user is login , hide the register button and show the logout button 
+window.addEventListener('DOMContentLoaded', function(){
+    const regDiv = document.getElementById('register');
+    if (regDiv) {
+        if (localStorage.getItem('email')) {
+            regDiv.style.display = 'none';
+            document.querySelector('.logout').style.display = 'block';
+        }
+    }
+});
+
+// this code is for show more and show less button in home page
 let btnShowMore = document.getElementById("show-more");
 let moreContents = document.querySelectorAll(".more");
 
